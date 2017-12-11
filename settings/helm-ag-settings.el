47761efbdc2cfdc9216d3f396a1e0dd156bea4b6
leeewl <33928880+leeewl@users.noreply.github.com>
2017-12-11 18:29:55 +0800

    Delete helm-ag-settings.el

--- settings/helm-ag-settings.el
+++
@@ -1,8 +0,0 @@
-
-;; requirement : ag
-;; the path of ag.exe is f:\msys64\mingw64\bin
-(setq exec-path (cons "f:/msys64/mingw64/bin" exec-path))
-
-;; command helm-do-ag
-
-(provide 'helm-ag-settings)
