e86275f28a07681495859149d8b6b400f141ab2e
leeewl <33928880+leeewl@users.noreply.github.com>
2017-12-12 14:39:05 +0800

    Update custom-functions.el

--- .emacs.d/settings/custom-functions.el
+++ .emacs.d/settings/custom-functions.el
@@ -1,6 +1,16 @@
 ;----------------------;
 ;;; Custom Functions ;;;
 ;----------------------;
+(defun system-is-mac ()
+  (interactive)
+  (string-equal system-type "darwin"))
 
+(defun system-is-linux ()
+  (interactive)
+  (string-equal system-type "gnu/linux"))
+
+(defun system-is-windows()
+  (interactive)
+  (string-equal system-type "windows-nt"))
 
 (provide 'custom-functions)
